c524a5178390ef9bd0e28e11b5a023509e160377
JudeNL <pjm733@mun.ca>
2016-04-04 10:21:06 -0230

    Update random_dense_pack_Seed_Restore.py

--- random_dense_pack_Seed_Restore.py
+++ random_dense_pack_Seed_Restore.py
@@ -29,10 +29,10 @@ steps = 10000
 #Parameters for Simulation Tree
 "Boxsize"
 global Boxsize
-Boxsize  = 10		#Size=10, ~100 particles; Size=30, ~4000 particles
+Boxsize  = 10	
 "Poisson"
 global poisson
-poisson = 0.5
+poisson = 0.3
 "Distfactor"
 distfact = 1.2
 "Porosity"
@@ -84,7 +84,7 @@ def sceneset( damp , yng , dns , tphi , ktdkn , brkn , alp , bet , u , distfact 
 "Sets up simulation with the specidied parameters"
 def simmake( boxsize , poros ):
       "Generating the packing"
-      genr = MinMaxSphereGenerator( dRange = ( 2 , 2 ) )		###
+      genr = MinMaxSphereGenerator( dRange = ( 2 , 2 ) )		
       pred = woo.pack.inAlignedBox( ( -3 , -boxsize/2 , -boxsize/2 ) , ( boxsize + 3 , boxsize/2 , boxsize/2 ) )
       pack = woo.pack.randomDensePack2( pred , genr , porosity = poros )
       pack.cellSize = ( 0 , 0 , 0 )
@@ -95,7 +95,11 @@ def simmake( boxsize , poros ):
             p.blocked = 'XYZ'
             
       "Adding the walls"
-      Scene.dem.par.add([ woo.utils.wall( position = ( 0 , 0 , 0 ) , axis = 0 , mat = Ice ) , woo.utils.wall( position = ( boxsize , 0 , 0 ) , axis = 0 , mat = Ice ) ], nodes = True|False )
+#      Scene.dem.par.add([ woo.utils.wall( position = ( 0 , 0 , 0 ) , axis = 0 , mat = Ice ) , woo.utils.wall( position = ( boxsize , 0 , 0 ) , axis = 0 , mat = Ice ) ], nodes = True|False )
+#      Scene.dem.par.add([ woo.utils.wall( position = ( 0 , -boxsize/2 , 0 ) , axis = 1 , mat = Ice ) , woo.utils.wall( position = ( 0 , boxsize/2 , 0 ) , axis = 1 , mat = Ice ) ], nodes = True|False )
+#      Scene.dem.par.add([ woo.utils.wall( position = ( 0 , 0 , -boxsize/2 ) , axis = 2 , mat = Ice ) , woo.utils.wall( position = ( 0 , 0 , boxsize/2 ) , axis = 2 , mat = Ice ) ], nodes = True|False )
+      Scene.dem.par.add(woo.triangulated.box( dim = ((boxsize+6),boxsize,boxsize), center = ((boxsize/2),0,0), which=(0,1,1,0,1,1) ) )
+      Scene.dem.par.add(woo.triangulated.box( dim = ((boxsize+6),boxsize,boxsize), center = ((boxsize/2),0,0), which=(1,0,0,1,0,0) ) )
       "Collecting nodes and returning wall"
 #      Scene.dem.collectNodes()		#Added to Scene.dem.par.add above (nodes = True|False)
       return Scene.dem.par[-1]      
@@ -114,15 +118,15 @@ def impose( par ):
 def condition1():
 
       "Used for finding particles of interest"
-      temp1 = 6
-      temp2 = 4
+      temp1 = 5.2
+      temp2 = 4.8
       i = 1
-      while i<130:			#139, 3045
+      while i<136:			#136, 3020
             x, y, z = Scene.dem.par[i].shape.nodes[0].pos 
             if x < temp1:
                   if x > temp2:
-                        if y < 2:
-                              if y > -2:
+                        if y < 1:
+                              if y > -1:
                                     miny = i
                                     print (x,',', y ,',', z)
                                     print (miny)
@@ -148,13 +152,13 @@ def condition2():
                   "Writing initial row of information"
                   dataLog.writerow([ 'Force' , 'dL' , 'NPar' , 'F/A' , 'dL/L' , 'E_eff' , 'E_param' , 'dW' , 'dW/W' , 'dH' , 'dH/H' , 'nu_param' , 'nu_z' , 'nu_y' , 'nu_calc' ])
 
-                  x, delH1i, z = Scene.dem.par[119].shape.nodes[0].pos
-                  x, delH2i, z = Scene.dem.par[123].shape.nodes[0].pos
+                  x, delH1i, z = Scene.dem.par[100].shape.nodes[0].pos
+                  x, delH2i, z = Scene.dem.par[101].shape.nodes[0].pos
                   global Ho
                   Ho = (abs(delH1i) + abs(delH2i))
 
-                  x, y, delW1i = Scene.dem.par[5].shape.nodes[0].pos
-                  x, y, delW2i = Scene.dem.par[118].shape.nodes[0].pos
+                  x, y, delW1i = Scene.dem.par[100].shape.nodes[0].pos
+                  x, y, delW2i = Scene.dem.par[101].shape.nodes[0].pos
                   print (delW1i)
                   print (delW2i)
                   global Wo
@@ -169,18 +173,18 @@ def collect( stepstart ):
       
       Force = particle.f[0]
       delL = particle.pos[0] - Boxsize
-      x, delH1, z = Scene.dem.par[119].shape.nodes[0].pos
-      x, delH2, z = Scene.dem.par[123].shape.nodes[0].pos
+      x, delH1, z = Scene.dem.par[100].shape.nodes[0].pos
+      x, delH2, z = Scene.dem.par[101].shape.nodes[0].pos
       delH = (abs(delH2) + abs(delH1)) - Ho
-      x, y, delW1 = Scene.dem.par[5].shape.nodes[0].pos
-      x, y, delW2 = Scene.dem.par[118].shape.nodes[0].pos
+      x, y, delW1 = Scene.dem.par[100].shape.nodes[0].pos
+      x, y, delW2 = Scene.dem.par[101].shape.nodes[0].pos
       delW = (abs(delW2) + abs(delW1)) - Wo
       NPar = len(Scene.dem.par)-2
       FdivA = Force/(Boxsize*Boxsize)
-      E_eff = FdivA/(delL/Boxsize)
+      E_eff = FdivA/(delL/Boxsize)	
       EpsZ = delW/Wo
       EpsY = delH/Ho
-      EpsX = delL/(Boxsize+6)
+      EpsX = delL/(Boxsize+6)	
       nu_z = EpsZ/EpsX
       nu_y = EpsY/EpsX
       
